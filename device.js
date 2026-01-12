65082511b611299a3ae1f90190bd979819099ff8
pixel <squirreljedi777@gmail.com>
2026-01-12 17:15:59 -0600

    Update device.js

--- device.js
+++ device.js
@@ -1,11 +1,11 @@
 document.getElementById("mobileBtn").onclick = () => {
   localStorage.setItem("device", "mobile");
-  localStorage.setItem("theme", "default"); // or "neon", "horror", etc.
-  window.location.href = "home.html";
+  localStorage.setItem("theme", "default");
+  window.location.href = "home/index.html";
 };
 
 document.getElementById("pcBtn").onclick = () => {
   localStorage.setItem("device", "pc");
   localStorage.setItem("theme", "default");
-  window.location.href = "home.html";
+  window.location.href = "home/index.html";
 };
