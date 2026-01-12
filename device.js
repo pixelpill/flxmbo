877c3b45a50519119277119e8a2fabaf12e186d1
pixel <squirreljedi777@gmail.com>
2026-01-12 09:02:37 -0600

    Set default theme on device selection
    Added theme setting for mobile and PC buttons.

--- device.js
+++ device.js
@@ -1,9 +1,11 @@
 document.getElementById("mobileBtn").onclick = () => {
   localStorage.setItem("device", "mobile");
+  localStorage.setItem("theme", "default"); // or "neon", "horror", etc.
   window.location.href = "home.html";
 };
 
 document.getElementById("pcBtn").onclick = () => {
   localStorage.setItem("device", "pc");
+  localStorage.setItem("theme", "default");
   window.location.href = "home.html";
 };
