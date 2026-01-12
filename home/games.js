50fed1b31f950c97c20891160c69a104138af055
pixel <squirreljedi777@gmail.com>
2026-01-12 09:10:32 -0600

    Create games.js with game details
    Added a list of games with their details including name, image, link, category, and mobile support.

---
+++ home/games.js
@@ -0,0 +1,16 @@
+const games = [
+  {
+    name: "Drive Mad",
+    img: "images/drive-mad.png",
+    link: "games/drive-mad/index.html",
+    category: "racing",
+    mobileSupport: true
+  },
+  {
+    name: "FNAF",
+    img: "images/fnaf.png",
+    link: "games/fnaf/index.html",
+    category: "horror",
+    mobileSupport: true
+  }
+];
