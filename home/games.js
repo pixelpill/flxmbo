f735436b38dbdd63475f142b77b5505ea2f4cb85
pixel <squirreljedi777@gmail.com>
2026-01-12 20:05:42 -0600

    Update game image paths and links for consistency

--- home/games.js
+++ home/games.js
@@ -1,23 +1,23 @@
 const games = [
   {
     name: "Drive Mad",
-    img: "images/drive-mad.png",
-    link: "games/drive-mad/index.html",
+    img: "../images/drive-mad.png",
+    link: "../games/drive-mad/index.html",
     category: "racing",
     mobileSupport: true
   },
   {
     name: "FNAF",
-    img: "images/fnaf.png",
-    link: "games/fnaf/index.html",
+    img: "../images/fnaf.png",
+    link: "../games/fnaf/index.html",
     category: "horror",
     mobileSupport: true
-  }
-    {
+  },
+  {
     name: "Monster Tracks",
-    img: "images/monster-tracks.jpeg",
-    link: "SingleFileGames/Monster-Tracks.html",
+    img: "../images/monster-tracks.jpeg",
+    link: "../SingleFileGames/Monster-Tracks.html",
     category: "racing",
     mobileSupport: true
-  },
+  }
 ];
